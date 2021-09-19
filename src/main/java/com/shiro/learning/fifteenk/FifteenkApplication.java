28e9ed068438b19920ab7b0d0a29d921a74c75f1
Limit-Beyond <33424641+limit-beyond@users.noreply.github.com>
2021-09-20 07:42:22 +0800

    项目初始化

---
+++ src/main/java/com/shiro/learning/fifteenk/FifteenkApplication.java
@@ -0,0 +1,13 @@
+package com.shiro.learning.fifteenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shiro/learning/fifteenk/FifteenkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiro.learning.fifteenk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifteenkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
